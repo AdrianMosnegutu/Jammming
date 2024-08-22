9bfd54a4b33a740e0aa1552cb4e12ec928c563ff
Moșneguțu Adrian-Ioan <146835839+MosnegutuAdrian@users.noreply.github.com>
2024-08-22 14:23:39 +0300

    add Main component

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,5 +1,34 @@
-const Main = () => {
-  return <main></main>;
+import PropTypes from "prop-types";
+import { useContext } from "react";
+import { Tracklist } from ".";
+import TracklistContext from "../../contexts/TracklistContext";
+import { PlaylistCreator } from "../PlaylistCreator";
+
+const Main = ({ searchResults }) => {
+  const { tracklist } = useContext(TracklistContext);
+
+  return (
+    <main>
+      <Tracklist trackObjectArray={searchResults} />
+      <div>
+        <PlaylistCreator />
+        <Tracklist trackObjectArray={tracklist} />
+      </div>
+    </main>
+  );
+};
+
+Main.propTypes = {
+  searchResults: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      uri: PropTypes.string.isRequired,
+      coverArt: PropTypes.string,
+      name: PropTypes.string.isRequired,
+      artists: PropTypes.arrayOf(PropTypes.string).isRequired,
+      album: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
 };
 
 export default Main;
--- src/pages/Jammming.jsx
+++ src/pages/Jammming.jsx
@@ -1,12 +1,16 @@
 import { useState } from "react";
+import { Main } from "../components/Main";
 import { Navigation } from "../components/Navigation";
 import { ProfileProvider } from "../contexts/ProfileContext";
 import { SearchProvider } from "../contexts/SearchContext";
+import { TracklistProvider } from "../contexts/TracklistContext";
 import mockProfile from "../mock/mockProfile.json";
+import mockTrack from "../mock/mockTrack.json";
 
 const Jammming = () => {
   const [searchQuery, setSearchQuery] = useState("");
   const [filter, setFilter] = useState("track");
+  const [searchResults, setSearchResults] = useState([mockTrack, mockTrack]);
 
   return (
     <ProfileProvider mockProfile={mockProfile}>
@@ -18,6 +22,9 @@ const Jammming = () => {
       >
         <Navigation />
       </SearchProvider>
+      <TracklistProvider>
+        <Main searchResults={searchResults} />
+      </TracklistProvider>
     </ProfileProvider>
   );
 };
