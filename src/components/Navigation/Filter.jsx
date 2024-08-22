b44dce29a45525bf3944c748b65c0f20eae93b34
Moșneguțu Adrian-Ioan <146835839+MosnegutuAdrian@users.noreply.github.com>
2024-08-22 13:02:07 +0300

    add Filter component

--- src/components/Navigation/Filter.jsx
+++ src/components/Navigation/Filter.jsx
@@ -1,5 +1,17 @@
-const Filter = () => {
-  return <select></select>;
+import PropTypes from "prop-types";
+
+const Filter = ({ filter, onFilterChange }) => (
+  <select value={filter} onChange={(e) => onFilterChange(e.target.value)}>
+    <option value="track">Tracks</option>
+    <option value="artist">Artists</option>
+    <option value="album">Albums</option>
+    <option value="genre">Genres</option>
+  </select>
+);
+
+Filter.propTypes = {
+  filter: PropTypes.string.isRequired,
+  onFilterChange: PropTypes.func.isRequired,
 };
 
 export default Filter;
