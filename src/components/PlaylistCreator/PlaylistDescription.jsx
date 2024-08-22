453ad15137cfcac796bbc271da6b9cb3f7ae678e
Moșneguțu Adrian-Ioan <146835839+MosnegutuAdrian@users.noreply.github.com>
2024-08-22 14:12:51 +0300

    add PlaylistDescription component

--- src/components/PlaylistCreator/PlaylistDescription.jsx
+++ src/components/PlaylistCreator/PlaylistDescription.jsx
@@ -1,7 +1,20 @@
-const PlaylistDescription = () => {
+import PropTypes from "prop-types";
+
+const PlaylistDescription = ({ description, onDescriptionChange }) => {
   return (
-    <textarea name="playlistDescription" id="playlistDescription"></textarea>
+    <textarea
+      name="playlistDescription"
+      id="playlistDescription"
+      placeholder="Write description..."
+      value={description}
+      onChange={(e) => onDescriptionChange(e.target.value)}
+    />
   );
+};
+
+PlaylistDescription.propTypes = {
+  description: PropTypes.string.isRequired,
+  onDescriptionChange: PropTypes.func.isRequired,
 };
 
 export default PlaylistDescription;
