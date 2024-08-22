3fa03f71d20d82746e09e447b3c18b00e8227300
Moșneguțu Adrian-Ioan <146835839+MosnegutuAdrian@users.noreply.github.com>
2024-08-22 13:04:53 +0300

    add ProfileDisplay component

--- src/components/Navigation/ProfileDisplay.jsx
+++ src/components/Navigation/ProfileDisplay.jsx
@@ -1,5 +1,23 @@
+import { useContext } from "react";
+import ProfileContext from "../../contexts/ProfileContext";
+
 const ProfileDisplay = () => {
-  return <div></div>;
+  const { profile } = useContext(ProfileContext);
+
+  const handleLogOut = () => {
+    localStorage.removeItem("access_token");
+    window.location = "/";
+  };
+
+  return (
+    <div>
+      <div>
+        {profile && <span>{profile.username}</span>}
+        <button onClick={handleLogOut}>Log out</button>
+      </div>
+      {profile && profile.icon && <img src={profile.icon} alt="profile icon" />}
+    </div>
+  );
 };
 
 export default ProfileDisplay;
