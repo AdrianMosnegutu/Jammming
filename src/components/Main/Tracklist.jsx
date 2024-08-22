7a33a0c9702b4be4f82ba83b1dfd62a77150733e
Moșneguțu Adrian-Ioan <146835839+MosnegutuAdrian@users.noreply.github.com>
2024-08-22 13:57:10 +0300

    add Tracklist component

--- src/components/Main/Tracklist.jsx
+++ src/components/Main/Tracklist.jsx
@@ -1,5 +1,25 @@
-const Tracklist = () => {
-  return <ul></ul>;
+import PropTypes from "prop-types";
+import { Track } from "../Track";
+
+const Tracklist = ({ trackObjectArray }) => (
+  <ul>
+    {trackObjectArray.map((trackObject) => (
+      <Track key={trackObject.id} trackObject={trackObject} />
+    ))}
+  </ul>
+);
+
+Tracklist.propTypes = {
+  trackObjectArray: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      uri: PropTypes.string.isRequired,
+      coverArt: PropTypes.string,
+      name: PropTypes.string.isRequired,
+      artists: PropTypes.arrayOf(PropTypes.string).isRequired,
+      album: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
 };
 
 export default Tracklist;
