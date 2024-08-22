0ece16a4ab7885cf81f4a970da22dc80b5b4fba8
Moșneguțu Adrian-Ioan <146835839+MosnegutuAdrian@users.noreply.github.com>
2024-08-22 14:16:55 +0300

    add PlaylistPrivacy component

--- src/components/PlaylistCreator/PlaylistPrivacy.jsx
+++ src/components/PlaylistCreator/PlaylistPrivacy.jsx
@@ -1,5 +1,35 @@
-const PlaylistPrivacy = () => {
-  return <fieldset></fieldset>;
+import PropTypes from "prop-types";
+
+const PlaylistPrivacy = ({ isPublic, onPrivacyChange }) => {
+  return (
+    <fieldset>
+      <div>
+        <input
+          type="radio"
+          name="privacy"
+          id="public"
+          checked={isPublic}
+          onChange={() => onPrivacyChange(true)}
+        />
+        <label htmlFor="public">Public</label>
+      </div>
+      <div>
+        <input
+          type="radio"
+          name="privacy"
+          id="private"
+          checked={!isPublic}
+          onChange={() => onPrivacyChange(false)}
+        />
+        <label htmlFor="private">Private</label>
+      </div>
+    </fieldset>
+  );
+};
+
+PlaylistPrivacy.propTypes = {
+  isPublic: PropTypes.bool.isRequired,
+  onPrivacyChange: PropTypes.func.isRequired,
 };
 
 export default PlaylistPrivacy;
