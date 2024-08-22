653317fbff970dde9a68af8ff29709d4531dc258
Moșneguțu Adrian-Ioan <146835839+MosnegutuAdrian@users.noreply.github.com>
2024-08-22 14:09:25 +0300

    add PlaylistName component

--- src/components/PlaylistCreator/PlaylistName.jsx
+++ src/components/PlaylistCreator/PlaylistName.jsx
@@ -1,5 +1,22 @@
-const PlaylistName = () => {
-  return <input type="text" name="playlistName" id="playlistName" />;
+import PropTypes from "prop-types";
+
+const PlaylistName = ({ name, onNameChange }) => {
+  return (
+    <input
+      type="text"
+      name="playlistName"
+      id="playlistName"
+      placeholder="Playlist name"
+      required
+      value={name}
+      onChange={(e) => onNameChange(e.target.value)}
+    />
+  );
+};
+
+PlaylistName.propTypes = {
+  name: PropTypes.string.isRequired,
+  onNameChange: PropTypes.func.isRequired,
 };
 
 export default PlaylistName;
