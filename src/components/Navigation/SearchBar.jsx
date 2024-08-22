2aebfb86143b8dd6871891ded73a8e7b2b346d54
Moșneguțu Adrian-Ioan <146835839+MosnegutuAdrian@users.noreply.github.com>
2024-08-22 13:02:37 +0300

    add SearchBar component

--- src/components/Navigation/SearchBar.jsx
+++ src/components/Navigation/SearchBar.jsx
@@ -1,5 +1,37 @@
+import { useContext, useState } from "react";
+import { FaSearch } from "react-icons/fa";
+import { Filter, InputField } from ".";
+import SearchContext from "../../contexts/SearchContext";
+
 const SearchBar = () => {
-  return <form></form>;
+  const {
+    searchQuery: globalSearchQuery,
+    setSearchQuery: onSearchQuerySubmit,
+    filter: globalFilter,
+    setFilter: onFilterSubmit,
+  } = useContext(SearchContext);
+
+  const [localSearchQuery, setLocalSearchQuery] = useState(globalSearchQuery);
+  const [localFilter, setLocalFilter] = useState(globalFilter);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSearchQuerySubmit(localSearchQuery);
+    onFilterSubmit(localFilter);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <InputField
+        searchQuery={localSearchQuery}
+        onSearchQueryChange={setLocalSearchQuery}
+      />
+      <Filter filter={localFilter} onFilterChange={setLocalFilter} />
+      <button type="submit">
+        <FaSearch />
+      </button>
+    </form>
+  );
 };
 
 export default SearchBar;
