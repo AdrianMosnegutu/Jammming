989ceabe4639879c2f2f20cb8429db7ad25bdb9a
Moșneguțu Adrian-Ioan <146835839+MosnegutuAdrian@users.noreply.github.com>
2024-08-22 13:02:24 +0300

    add InputField component

--- src/components/Navigation/InputField.jsx
+++ src/components/Navigation/InputField.jsx
@@ -1,5 +1,19 @@
-const InputField = () => {
-  return <input type="text" name="inputField" id="inputField" />;
+import PropTypes from "prop-types";
+
+const InputField = ({ searchQuery, onSearchQueryChange }) => (
+  <input
+    type="text"
+    name="inputField"
+    id="inputField"
+    placeholder="Search..."
+    value={searchQuery}
+    onChange={(e) => onSearchQueryChange(e.target.value)}
+  />
+);
+
+InputField.propTypes = {
+  searchQuery: PropTypes.string.isRequired,
+  onSearchQueryChange: PropTypes.func.isRequired,
 };
 
 export default InputField;
