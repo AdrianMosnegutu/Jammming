3bb3e9f74207ebbe4d24912a5350b46d9957a673
Moșneguțu Adrian-Ioan <146835839+MosnegutuAdrian@users.noreply.github.com>
2024-08-22 13:06:20 +0300

    add Navigation component

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,5 +1,13 @@
-const Navigation = () => {
-  return <nav></nav>;
-};
+import { ProfileDisplay, SearchBar } from ".";
+
+const Navigation = () => (
+  <nav>
+    <h1>
+      Ja<em>mmm</em>ing
+    </h1>
+    <SearchBar />
+    <ProfileDisplay />
+  </nav>
+);
 
 export default Navigation;
