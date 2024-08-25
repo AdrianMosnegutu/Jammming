bb8cd5c639cd8fc228ab5aaff6c0f25e217803b1
Moșneguțu Adrian-Ioan <146835839+MosnegutuAdrian@users.noreply.github.com>
2024-08-25 15:57:11 +0300

    style responsive LandingPage

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -3,22 +3,24 @@ import { requestAuthorization } from "../services/authenticator";
 
 const LandingPage = () => {
   return (
-    <header className="text-light-main flex flex-col items-center justify-center gap-28">
-      <div className="flex flex-col items-center gap-10 p-20">
-        <h1 className="text-9xl">
-          Ja<em className="text-spotify-green not-italic">mmm</em>ing
+    <header className="flex flex-col items-center justify-center gap-10 text-light-main lg:gap-20 xl:gap-28">
+      <div className="flex flex-col items-center gap-4 p-10 lg:gap-7 lg:p-20 xl:gap-10">
+        <h1 className="text-5xl lg:text-7xl xl:text-9xl">
+          Ja<em className="not-italic text-spotify-green">mmm</em>ing
         </h1>
-        <p className="text-4xl">
+        <p className="text-center text-base lg:text-2xl xl:text-4xl">
           Build an in-app playlist and export it directly to your Spotify
           account!
         </p>
       </div>
       <button
-        className="text-spotify-green border-spotify-green hover:bg-spotify-green hover:text-dark-main flex items-center gap-4 rounded-full border px-8 py-4 transition ease-in"
+        className="flex items-center gap-2 rounded-full border border-spotify-green px-4 py-2 text-spotify-green transition ease-in hover:bg-spotify-green hover:text-dark-main lg:gap-3 lg:px-6 lg:py-3 xl:gap-4 xl:px-8 xl:py-4"
         onClick={requestAuthorization}
       >
-        <FaSpotify size={30} />
-        <span className="text-3xl">Log in to Spotify</span>
+        <FaSpotify className="scale-75 lg:scale-90 xl:scale-100" size={30} />
+        <span className="text-base lg:text-xl xl:text-3xl">
+          Log in to Spotify
+        </span>
       </button>
     </header>
   );
